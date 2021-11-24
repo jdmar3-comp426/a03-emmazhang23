3ea50a1f386bc60e7bc7eaa0baca2f80ec8f26a2
Emma <ezpanxy@live.unc.edu>
2021-11-23 20:44:30 -0500

    sss

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -24,7 +24,7 @@ export function searchHighPower(car_data, minHorsepower, minTorque) {
         }
   
         return acc
-      }, []).sort((a, b) => (a.horsepower < b.horsepower) ? 1 : -1)
+      }, []).sort((a, b) => (a.horsepower > b.horsepower) ? 1 : -1)
 
 }
 
